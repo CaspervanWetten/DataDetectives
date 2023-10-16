9926b62534f922a503a216ee5cc5db0565b7fa26
Casper van Wetten <c.o.vanwetten@gmail.com>
2023-10-16 14:04:08 +0200

    Cleaned up the loose energy files, updated CCSD to actually work (woops), pushed the new temperature data on month basis

--- dash.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash
-import dash_leaflet as dl
-
-app = Dash()
-app.layout = dl.Map(dl.TileLayer(), center=[56,10], zoom=6, style={'height': '50vh'})
-
-if __name__ == '__main__':
-    app.run_server()
