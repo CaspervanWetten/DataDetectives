72b257b5557668154ac160b2beed0e85bf7cb5f9
Casper van Wetten <c.o.vanwetten@gmail.com>
2023-10-30 20:25:52 +0100

    Updated to include the full pipeline, also added comments
    Read that 'the full pipeline' means the second function. Note that all the imperatively programmed code has been removed.
    
    Also added comments to clear up whats happening

--- dashboard/Electricty.py
+++ dashboard/Electricty.py
@@ -1,126 +1,51 @@
 import pycountry
 import eurostat as es
-#import matplotlib.pyplot as plt
+import pandas as pd
 
-def MonthlyElectricity():
+def MonthlyElectricity(csv=False):
     try: 
-        # Specificy the pivotal columns, geo\\time = country, indic=kind of energy
-        id_vars = ['geo\\TIME_PERIOD', 'indic']
-        # The indic column uses internal EU indicators, we'll translate them to what we want with this dictionary
+        # Eurostat uses 'indic' with internal identifiers, this is the dictionary (pun intended) that translates those to a language we normies understand
         indicator_dictionary = {"IS-PEL-GWH" : "Production", "IS-CEL-GWH" : "Consumption", "IS-IEL-GWH" : "Imports"}
+        # Use this list to translate the alpha_2 country codes into the alpha_3 used by Dash
         alpha2_to_alpha3 = {country.alpha_2: country.alpha_3 for country in pycountry.countries}
 
-
-        df = es.get_data_df("EI_ISEN_M")
-        df.drop(columns=['freq','s_adj'], inplace=True)
-        df = df.melt(id_vars=id_vars, var_name='Date', value_name='GWH')
-        df = df[df['indic'].isin(indicator_dictionary.keys())]
-        df['indic'] = df['indic'].replace(indicator_dictionary)
-        df = df.dropna(subset=['GWH'])
-        df['geo\\TIME_PERIOD'] = df['geo\\TIME_PERIOD'].map(alpha2_to_alpha3)
+        
+        df = es.get_data_df("EI_ISEN_M") #downloads the dataset 
+        df['Country'] = df['geo\\TIME_PERIOD'].map(alpha2_to_alpha3) #Add the alpha_3 country codes to column "country"
+        df = df[df['indic'].isin(indicator_dictionary.keys())] #Filter out all the indics not in our dictionary, we don't use these (this is data cleaning!), other indics include: Natural gas imports, motor spirit refinery (no i dont know what this is), inland delivieries of brown coal (which, as we all know, is much more dangerous than it's cuddly cousins brown and polar-coal)
+        df['Indicator'] = df['indic'].replace(indicator_dictionary) #Use the dictionary to replace the names
+        df.drop(columns=['freq','s_adj', 'geo\\TIME_PERIOD', 'indic'], inplace=True) #Drop the unnecesary columns (ALSO DATA CLEANING!)
+        df = df.melt(id_vars=['Country', 'Indicator'], var_name='Date', value_name='GWH') # flip the column from horizontal do vertical based around the Date column
+        #The following 3 lines split the Date column into a seperate Year and Month column
+        df['Date'] = pd.to_datetime(df['Date'])
+        df['Year'] = df['Date'].dt.year
+        df['Month'] = df['Date'].dt.month
+        df = df[["Country", "Year", "Month", "GWH", "Indicator"]] #I wanted to have all dataframes in (more or less) the same order. No I'm not autistic and thinking such questions is rude >:()
+        df = df.dropna(subset=['GWH', 'Country']) #Drop the ones of which we don't have data ('GWH') and which aren't part of our country list (The data also has aggregate EU data which converts to an empty Country cell after the alpha_2->3 mapping, we use this to filter those out)
+        if csv:
+            df.to_csv("csv/MonthlyElectricity.csv")
         return df
     except Exception as e:
-        print(f"quit with {e} as error")
+        print(f"quit with \n{e} \nas error")
 
 
-def TypesOfEnergy():
+def TypesOfEnergy(csv=False):
     try: 
-        # Specificy the pivotal columns, geo\\time = country, indic=kind of energy
-        id_vars = ['geo\\TIME_PERIOD', 'indic']
-        # The indic column uses internal EU indicators, we'll translate them to what we want with this dictionary
-        indicator_dictionary = {"C0000X0350-0370" : "Solid Fossil Fuels", "IS-CEL-GWH" : "Consumption", "IS-IEL-GWH" : "Imports"}
-        df = es.get_data_df("ten00122")
-        df.drop(columns=['freq','nrg_bal','unit'], inplace=True)
-       # df = df.melt(id_vars=id_vars, var_name='Date', value_name='GWH')
-       # df = df[df['indic'].isin(indicator_dictionary.keys())]
-       # df['indic'] = df['indic'].replace(indicator_dictionary)
-        #df.to_csv("ElectricityM.csv", index=False)
+        # Eurostat uses 'siec' with internal identifiers, this is the dictionary (pun intended) that translates those to a language we normies understand
+        value_mapping = {'C0000X0350-0370': 'Solid fossil fuels','C0350-0370': 'Manufactured gases','P1000': 'Peat and peat products','S2000': 'Oil shale and oil sands','O4000XBIO': 'Oil and petroleum products (excluding biofuel portion)','G3000': 'Natural gas','RA000': 'Renewables and biofuels','W6100_6220': 'Non-renewable waste','N900H': 'Nuclear heat','H8000': 'Heat','E7000': 'Electricity','TOTAL': 'Total'}
+        # Use this list to translate the alpha_2 country codes into the alpha_3 used by Dash
+        alpha2_to_alpha3 = {country.alpha_2: country.alpha_3 for country in pycountry.countries}
+
+        df = es.get_data_df("ten00122") #downloads the dataset 
+        df['Country'] = df['geo\\TIME_PERIOD'].map(alpha2_to_alpha3) #Add the alpha_3 country codes to column "country"
+        df['Indicator'] = df['siec'].replace(value_mapping) #Use the dictionary to replace the names, we use ALL indicators for this dataset
+        df.drop(columns=['freq','nrg_bal','unit', 'geo\\TIME_PERIOD', 'siec'], inplace=True) #Drop the unnecesary columns (ALSO DATA CLEANING!)
+        df = df.melt(id_vars=['Indicator', 'Country'], var_name='Year', value_name='KTOE') # flip the column from horizontal do vertical based around the Date column
+        df = df[["Country", "Year", "KTOE", "Indicator"]] #I wanted to have all dataframes in (more or less) the same order. No I'm not autistic and thinking such questions is rude >:()
+        df = df.dropna(subset=['KTOE', 'Country'])#Drop the ones of which we don't have data ('KTOE') and which aren't part of our country list (The data also has aggregate EU data which converts to an empty Country cell after the alpha_2->3 mapping, we use this to filter those out)
+        if csv: #Soms wil je een CSV, en soms niet :)))))
+            df.to_csv("csv/TypesOfElectricity.csv")
         return df
     except Exception as e:
-        return f"quit with {e} as error"
-
-# df2 = TypesOfEnergy()
-# #print(df2)
-# df2.fillna(0, inplace=True)
-# #change column name to country and also energy_kind
-# df2 = df2.rename(columns={'geo\\TIME_PERIOD': 'country'})
-# df2 = df2.rename(columns={'siec': 'energy'})
-
-# belgiumdf = df2[df2['country'] == 'AL']
-# unique_values = belgiumdf['energy'].unique()
-
-# # Initialize a dictionary to store the summed energy values for each unique value
-# summed_energy_data = {}
-
-# # Loop through unique values and calculate the sum of energy for each
-# for value in unique_values:
-#     summed_energy_data[value] = belgiumdf[belgiumdf['energy'] == value][['2011']].sum()
-    
-# # Print the summed energy for each unique value
-# for value, total_energy in summed_energy_data.items():
-#     print(f"Total energy for \n'{value}': {total_energy}")
-
-# total_energy_sum = sum(summed_energy_data.values())
-# print(f"Sum of all total energies: {total_energy_sum}")
-
-# # Define the reverse conversion factor
-# reverse_conversion_factor = 11.631  # GWh per ktoe
-
-# # Convert from ktoe to GWh
-# energy_gwh = total_energy_sum / reverse_conversion_factor
-
-# # Print the result
-# print(f"{total_energy_sum} ktoe is approximately equal to {energy_gwh} GWh")
-
-# #remap all energy values 
-# unique_energy_values = df2['energy'].unique()
-
-# # Print the unique energy values
-# for energy in unique_energy_values:
-#     print(energy)
-
-# # Define a mapping of old values to new values
-# value_mapping = {
-#     'C0000X0350-0370': 'Solid fossil fuels',
-#     'C0350-0370': 'Manufactured gases',
-#     'P1000': 'Peat and peat products',
-#     'S2000': 'Oil shale and oil sands',
-#     'O4000XBIO': 'Oil and petroleum products (excluding biofuel portion)',
-#     'G3000': 'Natural gas',
-#     'RA000': 'Renewables and biofuels',
-#     'W6100_6220': 'Non-renewable waste',
-#     'N900H': 'Nuclear heat',
-#     'H8000': 'Heat',
-#     'E7000': 'Electricity',
-#     'TOTAL': 'Total'
-# }
-# # Replace the values in the DataFrame
-# df2['energy'] = df2['energy'].replace(value_mapping)
-
-# #remove the total attribute for our plots
-# df2 = df2[df2['energy'] != 'Total']
-# print(df2.head(10))
-
-# df_pivot = df2.melt(id_vars=['energy', 'country'], var_name='year', value_name='consumption')
-
-# print(df_pivot)
-# grouped = df_pivot.groupby(['energy', 'year'])['consumption'].sum().unstack()
-
-# energy_groups = df2.groupby('energy')
-
-# # Initialize the plot
-# plt.figure()
-
-# for name, group in energy_groups:
-#     plt.bar(group.columns[2:], group.iloc[0, 2:], label=name)
-
-# # Customize the plot
-# plt.xlabel('Years')
-# plt.ylabel('Energy Consumption (GWh)')
-# plt.title('Energy Consumption by Type (2011-2021)')
-# plt.xticks(rotation=45)
-# plt.legend()
-
-# # Show the plot
-# plt.show()
+        return f"quit with \n{e} \nas error"
 
