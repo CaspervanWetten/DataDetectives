1a1521177df3764fdc84f9bf9d20d7a4f7c113bf
Casper van Wetten <c.o.vanwetten@gmail.com>
2023-10-30 13:51:00 +0100

    Reverted app.py to pre-Thomas

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,9 +9,8 @@ from os import listdir
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-from Charts import update_choropleth
+from Charts import Choropleth
 from Database import Database
-import os
 
 app = dash.Dash(__name__)
 server = app.server
@@ -20,35 +19,43 @@ app.scripts.config.serve_locally = True
 app.title="We testen wat!"
 app.description="Dit is een omschrijving 0_O"
 
+db = Database()
+df = db._fetch_data("electricity", False, "Country", "GWH", Year="=2010")
+possible_years = sorted(db._fetch_data("electricity", True, "Year"), key=int)
+print(df.head())
+selected_year = 2018
+selected_indicator = "consumption"
 
-db=Database()
 
-
-LIST = os.listdir('C:\\Users\\valke\\OneDrive\\Documenten\\GitHub\\DataDetectives\\dashboard\\data')
-
-for file in LIST:
-    path = os.path.join('C:\\Users\\valke\\OneDrive\\Documenten\\GitHub\\DataDetectives\\dashboard\\data', file)
-    with open(path):
-        df = pd.read_csv(path)
-    a = file.split(".")[0]
-    db._load_data(a=df)
-    print(file.split(".")[0])    
-    
-    
-    
-
-population_df = db._fetch_data("population")
-print(population_df.head(10))
-
-app.layout = html.Div([
+app.layout = html.Div(children=[
     html.H1('Hello Dash!'),
-    html.P('Dash converts this kind of semi-python into html'),
-    html.Div([
-        html.Div('Lorem ipsum 1', className='col'),
-        html.Div('Lorem ipsum 2', className='col col-lg-2')
-    ], className='row')
-], className='container')
+    html.Div(className='row', children=[
+        html.Div(className='col-8', children=[
+            html.Div(f'GWH {selected_indicator} distribution in Europe for {selected_year}', className="GWH"),
+            dcc.Graph(className="choropleth", id="choropleth", figure=Choropleth(df))
+        ]),
+        html.Div('Lorem ipsum 2', className='col-4')
+    ]),
+     html.Div(className='row', children=[
+         html.Div(className="col-6", children=[
+             dcc.Slider(className="choroslider", id="chloroslider", value=selected_year, marks={year : str(year) for year in possible_years})
+        ]) 
+    ])
+])
+
+
+
+# @app.callback(
+#     Output(component_id='choropleth'),
+#     Input(component_id='chloroslider', component_property="value")
+# )
+# def update_choropleth(selected_year):
+#     df = db._fetch_data("electricity", False, "Country", "GWH", Year=f"={selected_year}")
+#     Choropleth(df)
 
 if __name__ == '__main__':
     print("Started")
     app.run_server(debug=True, host='172.19.0.3', port=5000)
+
+    
+# marks = []
