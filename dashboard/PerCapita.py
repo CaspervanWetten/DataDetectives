1a7bb7398d311f4471c6afdf604e2535f233f0bf
Casper van Wetten <c.o.vanwetten@gmail.com>
2023-11-08 13:21:21 +0100

    Per/cap works now

--- dashboard/PerCapita.py
+++ dashboard/PerCapita.py
@@ -1,15 +1,13 @@
 import pandas as pd
-from Database import Database
 
-def caculate_per_capita(population: pd.DataFrame, to_merge: pd.DataFrame, to_match = "gwh", group_by = "year"):
+def caculate_per_capita(population: pd.DataFrame, to_merge: pd.DataFrame):
     to_merge = to_merge[to_merge['indicator'] == 'Consumption']
-    gwh_avg = to_merge.groupby(group_by)[to_match].mean().reset_index()
-    per_cap = pd.merge(population, gwh_avg, on = ["year", "country"])
-    per_cap["per_capita"] = per_cap[to_match] / per_cap["population"]
+    to_merge = to_merge.groupby(['country', 'year']).agg({'gwh': 'sum'}).reset_index().round()
+    merged = pd.merge(population, to_merge, on=['country', 'year'], how='inner')
+    merged["per_capita"] = merged["gwh"] / merged["population"]
+    return merged
     
-    return per_cap
-    
-gwh = pd.read_csv("dashboard\csv\electricity_consumption.csv")
-pop = pd.read_csv("dashboard\csv\population.csv")
+gwh = pd.read_csv("dashboard/csv/electricity_consumption.csv")
+pop = pd.read_csv("dashboard/csv/population.csv")
 
-print(caculate_per_capita(pop, gwh))
+print(caculate_per_capita(pop, gwh).head(25))
